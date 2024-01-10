5a86c61a78e1fa5ae27ac35d6c8868b8c77c1bcb
Mathias <mgaj@tv2.dk>
2024-01-10 15:35:15 +0100

    Extracted library versions into Versions.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,19 +47,31 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    val composeBom = platform("androidx.compose:compose-bom:${Versions.COMPOSE_BOM_VERSION}")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+
+    // Android libraries
+    implementation("androidx.core:core-ktx:${Versions.ANDROIDX_CORE_VERSION}")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Versions.ANDROIDX_LIFECYCLE_VERSION}")
+    implementation("androidx.activity:activity-compose:${Versions.ANDROIDX_ACTIVITY_COMPOSE_VERSION}")
+
+    // Jetpack Compose
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Test implementations
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+
+    // Test implementations for Jetpack Compose
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,16 @@
+object Versions {
+    const val ANDROIDX_CORE_VERSION = "1.12.0"
+    const val ANDROIDX_CORE_SPLASHSCREEN_VERSION = "1.0.1"
+    const val ANDROIDX_LIFECYCLE_VERSION = "2.6.2"
+    const val ANDROIDX_ACTIVITY_COMPOSE_VERSION = "1.8.2"
+
+    // Jetpack Compose version
+    const val COMPOSE_BOM_VERSION = "2023.10.01"
+
+    const val RETROFIT_VERSION = "2.9.0"
+
+    // Test implementation versions
+    const val JUNIT_VERSION = "4.13.2"
+    const val ANDROIDX_JUNIT_VERSION = "1.1.5"
+    const val ANDROIDX_ESPRESSO_VERSION = "3.5.1"
+}
