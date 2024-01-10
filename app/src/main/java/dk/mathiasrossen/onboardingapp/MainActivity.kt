60cad6dcbc451a081b321937e9508dd42ed3735d
Mathias <mgaj@tv2.dk>
2024-01-10 13:34:42 +0100

    Added the splash screen

--- app/src/main/java/dk/mathiasrossen/onboardingapp/MainActivity.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package dk.mathiasrossen.onboardingapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             OnboardingAppTheme {
                 // A surface container using the 'background' color from the theme
