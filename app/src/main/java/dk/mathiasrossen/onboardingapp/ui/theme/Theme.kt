64cfa469cafa9ef4a097e2bf245570997ea38e12
Mathias <mgaj@tv2.dk>
2024-01-11 08:09:44 +0100

    Refactor theme colors

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/theme/Theme.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/theme/Theme.kt
@@ -1,48 +1,29 @@
 package dk.mathiasrossen.onboardingapp.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = BlueGray,
-    secondary = White,
-    tertiary = Orange
-)
-
 private val LightColorScheme = lightColorScheme(
-    primary = BlueGray,
-    secondary = White,
+    primary = BlueGrayDark,
+    secondary = Orange,
     tertiary = Orange,
+    primaryContainer = BlueGray,
+    onPrimaryContainer = White
 )
 
 @Composable
 fun OnboardingAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
