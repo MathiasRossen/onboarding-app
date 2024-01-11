a144dd74711a625c451448f61bfb875097739329
Mathias <mgaj@tv2.dk>
2024-01-11 14:41:54 +0100

    Renamed CoreInterceptor to AutthorizationInterceptor

--- app/src/main/java/dk/mathiasrossen/onboardingapp/services/AuthorizationInterceptor.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/services/AuthorizationInterceptor.kt
@@ -4,7 +4,7 @@ import dk.mathiasrossen.onboardingapp.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class CoreInterceptor: Interceptor {
+class AuthorizationInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
             .newBuilder()
