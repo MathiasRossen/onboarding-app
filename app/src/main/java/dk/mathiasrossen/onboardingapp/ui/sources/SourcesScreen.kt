6feac840b45d7d95e417c94185eb4d5a142eb4ad
Mathias <mgaj@tv2.dk>
2024-01-11 10:43:01 +0100

    Add 'SourcesScreen.kt'

---
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesScreen.kt
@@ -0,0 +1,9 @@
+package dk.mathiasrossen.onboardingapp.ui.sources
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SourcesScreen() {
+    Text(text = "Sources")
+}
