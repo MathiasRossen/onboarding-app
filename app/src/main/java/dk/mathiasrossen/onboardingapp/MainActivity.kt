c70663ba744e578ae2e744edb35a1482eb2f7be9
Mathias <mgaj@tv2.dk>
2024-01-11 10:37:28 +0100

    Added scaffolding to MainActivity

--- app/src/main/java/dk/mathiasrossen/onboardingapp/MainActivity.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/MainActivity.kt
@@ -1,17 +1,24 @@
 package dk.mathiasrossen.onboardingapp
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
+import dk.mathiasrossen.onboardingapp.navigation.Screen
+import dk.mathiasrossen.onboardingapp.ui.appbar.OnboardingBottomAppBar
+import dk.mathiasrossen.onboardingapp.ui.appbar.OnboardingTopAppBar
+import dk.mathiasrossen.onboardingapp.ui.sources.SourcesScreen
 import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,28 +26,34 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
+            val navController = rememberNavController()
+
             OnboardingAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Scaffold(
+                    topBar = {
+                        OnboardingTopAppBar()
+                    },
+                    bottomBar = {
+                        OnboardingBottomAppBar(navController)
+                    }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.Sources.route,
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable(Screen.Sources.route) {
+                            SourcesScreen()
+                        }
+                        composable(Screen.Favorites.route) {
+                            Text(text = "Favorites")
+                        }
+                        composable(Screen.About.route) {
+                            Text(text = "About")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OnboardingAppTheme {
-        Greeting("Android")
     }
 }
