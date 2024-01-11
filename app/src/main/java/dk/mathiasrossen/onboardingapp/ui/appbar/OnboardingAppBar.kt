9493c8021a1f84d2afd76a5487c5b19671342a76
Mathias <mgaj@tv2.dk>
2024-01-11 08:09:11 +0100

    Add OnboardingAppBar.kt

---
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/appbar/OnboardingAppBar.kt
@@ -0,0 +1,33 @@
+package dk.mathiasrossen.onboardingapp.ui.appbar
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OnboardingAppBar() {
+    TopAppBar(
+        title = {
+            Text("NewsApp")
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun TopAppBarPreview() {
+    OnboardingAppTheme {
+        OnboardingAppBar()
+    }
+}
