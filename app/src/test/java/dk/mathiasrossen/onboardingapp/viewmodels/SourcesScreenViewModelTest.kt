7f2fdb6902b815d855ee73158b696085cfd1e4cc
Mathias <mgaj@tv2.dk>
2024-01-15 10:21:32 +0100

    Added unit tests for SourcesScreenViewModel

---
+++ app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/SourcesScreenViewModelTest.kt
@@ -0,0 +1,47 @@
+package dk.mathiasrossen.onboardingapp.viewmodels
+
+import dk.mathiasrossen.onboardingapp.api.NewsApiService
+import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
+import dk.mathiasrossen.onboardingapp.models.NewsSource
+import dk.mathiasrossen.onboardingapp.ui.sources.SourcesScreenViewModel
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Assert
+import org.junit.Test
+import org.mockito.BDDMockito.given
+import org.mockito.kotlin.mock
+
+class SourcesScreenViewModelTest {
+    private val service = mock<NewsApiService>()
+    private lateinit var viewModel: SourcesScreenViewModel
+
+    @Test
+    fun init_onSucess_newsSourcesReturned() {
+        val mockNewsSourcesResponse = NewsSourcesResponse(
+                "ok",
+                listOf(
+                    newNewsSource("1", "HorseNews"),
+                    newNewsSource("2", "DonkeyNews")
+                )
+            )
+        given(service.getSources()).willReturn(Single.just(mockNewsSourcesResponse))
+
+        viewModel = createViewModel()
+
+        Assert.assertEquals(mockNewsSourcesResponse.sourcesSorted, viewModel.newsSources.value)
+    }
+
+    private fun createViewModel() = SourcesScreenViewModel(service, Schedulers.trampoline())
+
+    private fun newNewsSource(id: String, name: String): NewsSource {
+        return NewsSource(
+            id,
+            name,
+            "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos related to horses at Horsenews.com",
+            "https://horsenews.com/1",
+            "horses",
+            "en",
+            "us"
+        )
+    }
+}
