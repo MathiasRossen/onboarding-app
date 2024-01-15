596bef47641c658eb7643d893ca84df96de28809
Mathias <mgaj@tv2.dk>
2024-01-15 10:00:56 +0100

    Added uiScheduler to SourcesScreenViewModel instead of having it hardcoded

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesScreenViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesScreenViewModel.kt
@@ -5,12 +5,12 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dk.mathiasrossen.onboardingapp.api.NewsApiService
 import dk.mathiasrossen.onboardingapp.models.NewsSource
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.disposables.Disposable
 import javax.inject.Inject
 
 @HiltViewModel
-class SourcesScreenViewModel @Inject constructor(newsApiService: NewsApiService) : ViewModel() {
+class SourcesScreenViewModel @Inject constructor(newsApiService: NewsApiService, uiScheduler: Scheduler) : ViewModel() {
     private var disposable = Disposable.disposed()
 
     var newsSources = mutableStateOf(listOf<NewsSource>())
@@ -18,7 +18,7 @@ class SourcesScreenViewModel @Inject constructor(newsApiService: NewsApiService)
 
     init {
         disposable = newsApiService.getSources()
-            .observeOn(AndroidSchedulers.mainThread())
+            .observeOn(uiScheduler)
             .subscribe({ newsSourcesResponse ->
                 newsSources.value = newsSourcesResponse.sourcesSorted
             }) { /* error */ }
