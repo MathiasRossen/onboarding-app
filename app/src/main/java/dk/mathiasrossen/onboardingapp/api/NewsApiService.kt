918f0889618e17c603974e4612ae719bc06b438c
Mathias <mgaj@tv2.dk>
2024-01-16 12:11:59 +0100

    Added 'ArticlesResponse.kt'

--- app/src/main/java/dk/mathiasrossen/onboardingapp/api/NewsApiService.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/api/NewsApiService.kt
@@ -1,5 +1,6 @@
 package dk.mathiasrossen.onboardingapp.api
 
+import dk.mathiasrossen.onboardingapp.api.response_models.ArticlesResponse
 import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
@@ -10,7 +11,10 @@ interface NewsApiService {
     fun getSources(@Query("language") language: String = DEFAULT_LANGUAGE): Single<NewsSourcesResponse>
 
     @GET("v2/everything")
-    fun getArticlesFromSource(@Query("sources") source: String, @Query("language") language: String = DEFAULT_LANGUAGE)
+    fun getArticlesFromSource(
+        @Query("sources") source: String,
+        @Query("language") language: String = DEFAULT_LANGUAGE
+    ): Single<ArticlesResponse>
 
     companion object {
         private const val DEFAULT_LANGUAGE = "en"
