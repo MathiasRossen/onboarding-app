8ca7c817a24d3a3a1b0eec5465e7af889e66882b
Mathias <mgaj@tv2.dk>
2024-01-16 10:15:35 +0100

    Add authorAndPublishedAt helper function

--- app/src/main/java/dk/mathiasrossen/onboardingapp/models/Article.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/models/Article.kt
@@ -1,12 +1,16 @@
 package dk.mathiasrossen.onboardingapp.models
 
 data class Article(
-    val source: String,
-    val author: String,
+    val source: Source,
     val title: String,
+    val author: String,
     val description: String,
     val url: String,
     val urlToImage: String,
     val publishedAt: String,
     val content: String
-)
+) {
+    val authorAndPublishedAt get() = "$author - $publishedAt"
+
+    data class Source(val id: String?, val name: String)
+}
