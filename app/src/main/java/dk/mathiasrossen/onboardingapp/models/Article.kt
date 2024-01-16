452bc5e8ca4c639c9c1513f94d04f420bc540651
Mathias <mgaj@tv2.dk>
2024-01-16 12:03:58 +0100

    Added createSample companion function

--- app/src/main/java/dk/mathiasrossen/onboardingapp/models/Article.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/models/Article.kt
@@ -13,4 +13,17 @@ data class Article(
     val authorAndPublishedAt get() = "$author - $publishedAt"
 
     data class Source(val id: String?, val name: String)
+
+    companion object {
+        fun createSample(): Article = Article(
+            Article.Source("", ""),
+            "Look at my horse, my horse is amazing - You should definately check out my creature",
+            "John Doe",
+            "This horse can do a lot of fabolous tricks. The funny thing about my horse is that if you lick it, you get the taste of raisins.",
+            "",
+            "",
+            "2024-01-16 10:05",
+            ""
+        )
+    }
 }
