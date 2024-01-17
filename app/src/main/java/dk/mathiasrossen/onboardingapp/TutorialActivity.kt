99707c68a9a7678adc48e5ddd3a628498b9165ea
Mathias <mgaj@tv2.dk>
2024-01-17 14:59:42 +0100

    Move completeTutorial to view model

--- app/src/main/java/dk/mathiasrossen/onboardingapp/TutorialActivity.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/TutorialActivity.kt
@@ -1,9 +1,9 @@
 package dk.mathiasrossen.onboardingapp
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.addCallback
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,16 +12,13 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.edit
+import dagger.hilt.android.AndroidEntryPoint
 import dk.mathiasrossen.onboardingapp.ui.tutorial.TutorialPageOne
 import dk.mathiasrossen.onboardingapp.ui.tutorial.TutorialPageThree
 import dk.mathiasrossen.onboardingapp.ui.tutorial.TutorialPageTwo
-import dk.mathiasrossen.onboardingapp.utils.dataStore
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class TutorialActivity : AppCompatActivity() {
     private val pages = 3
 
@@ -31,6 +28,7 @@ class TutorialActivity : AppCompatActivity() {
 
         onBackPressedDispatcher.addCallback { /* Prevent user from popping the tutorial screen */ }
 
+        val tutorialActivityViewModel: TutorialActivityViewModel by viewModels()
         setContent {
             val pagerState = rememberPagerState { pages }
             HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { page ->
@@ -47,8 +45,8 @@ class TutorialActivity : AppCompatActivity() {
                         }
                     }
                     2 -> TutorialPageThree {
-                        coroutineScope.launch {
-                            completeTutorial()
+                        tutorialActivityViewModel.completeTutorial {
+                            finish()
                         }
                     }
                 }
@@ -56,27 +54,8 @@ class TutorialActivity : AppCompatActivity() {
         }
     }
 
-    private suspend fun completeTutorial() {
-        dataStore.edit { settings ->
-            val key = booleanPreferencesKey(TUTORIAL_COMPLETED)
-            settings[key] = true
-        }
-        finish()
-    }
-
     @OptIn(ExperimentalFoundationApi::class)
     private suspend fun PagerState.scrollToNextPage() {
         animateScrollToPage(currentPage + 1)
-    }
-
-    companion object {
-        private const val TUTORIAL_COMPLETED = "tutorial_completed"
-
-        fun isTutorialCompleted(context: Context): Flow<Boolean> {
-            val key = booleanPreferencesKey(TUTORIAL_COMPLETED)
-            return context.dataStore.data.map { preferences ->
-                preferences[key] ?: false
-            }
-        }
     }
 }
