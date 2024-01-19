41baa230533d8798455664cabea28dce747aec27
Mathias <mgaj@tv2.dk>
2024-01-19 10:09:20 +0100

    Add unit test for getArticlesFromSource

--- app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/ArticleListViewModelTest.kt
+++ app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/ArticleListViewModelTest.kt
@@ -3,32 +3,53 @@ package dk.mathiasrossen.onboardingapp.viewmodels
 import androidx.lifecycle.SavedStateHandle
 import dk.mathiasrossen.onboardingapp.api.NewsApiService
 import dk.mathiasrossen.onboardingapp.api.response_models.ArticlesResponse
+import dk.mathiasrossen.onboardingapp.models.Article
 import dk.mathiasrossen.onboardingapp.ui.articles.ArticleListViewModel
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.given
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 
 class ArticleListViewModelTest {
     private val sourceId = "123"
+    private val date = "2024-01-18"
     private val service = mock<NewsApiService>()
     private val scheduler = Schedulers.trampoline()
     private lateinit var viewModel: ArticleListViewModel
     private val savedStateHandle = SavedStateHandle().apply {
         set(ArticleListViewModel.SOURCE_ID_KEY, sourceId)
     }
+    private val mockArticleResponse = ArticlesResponse(
+        "ok",
+        2,
+        listOf(
+            Article.createSample(),
+            Article.createSample()
+        )
+    )
+
+    @Before
+    fun setup() {
+        given(service.getArticlesFromSource(eq(sourceId), any(), any(), eq(date))).willReturn(
+            Single.just(mockArticleResponse)
+        )
+        viewModel = createViewModel()
+    }
 
     @Test
     fun init_serviceGetsArticles() {
-        given(service.getArticlesFromSource(sourceId)).willReturn(
-            Single.just(ArticlesResponse("", 0, emptyList()))
-        )
+        verify(service).getArticlesFromSource(sourceId, from = date)
+    }
 
-        viewModel = createViewModel()
-
-        verify(service).getArticlesFromSource(sourceId)
+    @Test
+    fun init_onSuccess_articlesReturned() {
+        Assert.assertEquals(mockArticleResponse.articles, viewModel.articles.value)
     }
 
     private fun createViewModel() = ArticleListViewModel(service, scheduler, savedStateHandle)
