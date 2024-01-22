cb6c81139e18764cc5bef61aa7023ec1cd5e87f4
Mathias <mgaj@tv2.dk>
2024-01-22 08:20:38 +0100

    Add titleRes to enums

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/SortState.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/SortState.kt
@@ -1,7 +1,10 @@
 package dk.mathiasrossen.onboardingapp.ui.articles
 
-enum class SortState {
-    POPULAR_TODAY,
-    POPULAR_ALL_TIME,
-    NEWEST
+import androidx.annotation.StringRes
+import dk.mathiasrossen.onboardingapp.R
+
+enum class SortState(@StringRes val title: Int) {
+    POPULAR_TODAY(R.string.article_list_button_sort_popular_today),
+    POPULAR_ALL_TIME(R.string.article_list_button_sort_popular_all_time),
+    NEWEST(R.string.article_list_button_sort_newest)
 }
