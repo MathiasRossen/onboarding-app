80cfb968e02fa5684afe0c3494a5175a0f34db5d
Mathias <mgaj@tv2.dk>
2024-01-24 15:04:36 +0100

    Rename string resources from 'article_list_...' to 'articles_screen_...'

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/SortState.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/SortState.kt
@@ -4,7 +4,7 @@ import androidx.annotation.StringRes
 import dk.mathiasrossen.onboardingapp.R
 
 enum class SortState(@StringRes val title: Int) {
-    POPULAR_TODAY(R.string.article_list_button_sort_popular_today),
-    POPULAR_ALL_TIME(R.string.article_list_button_sort_popular_all_time),
-    NEWEST(R.string.article_list_button_sort_newest)
+    POPULAR_TODAY(R.string.articles_screen_button_sort_popular_today),
+    POPULAR_ALL_TIME(R.string.articles_screen_button_sort_popular_all_time),
+    NEWEST(R.string.articles_screen_button_sort_newest)
 }
