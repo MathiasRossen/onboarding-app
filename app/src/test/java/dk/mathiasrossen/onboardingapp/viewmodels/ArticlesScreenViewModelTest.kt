98a070dc5d4c462454a3a1be7f4458955ee604cb
Mathias <mgaj@tv2.dk>
2024-01-25 15:54:53 +0100

    Refactor ArticlesScreenViewModel to use ArticlesUseCase instead of NewsApiService

--- app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/ArticlesScreenViewModelTest.kt
+++ app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/ArticlesScreenViewModelTest.kt
@@ -2,10 +2,10 @@ package dk.mathiasrossen.onboardingapp.viewmodels
 
 import androidx.lifecycle.SavedStateHandle
 import dk.mathiasrossen.onboardingapp.api.NewsApiService
-import dk.mathiasrossen.onboardingapp.api.response_models.ArticlesResponse
-import dk.mathiasrossen.onboardingapp.models.Article
+import dk.mathiasrossen.onboardingapp.data.article.Article
 import dk.mathiasrossen.onboardingapp.ui.articles.list.ArticlesScreenViewModel
 import dk.mathiasrossen.onboardingapp.ui.articles.list.SortState
+import dk.mathiasrossen.onboardingapp.use_cases.ArticlesUseCase
 import dk.mathiasrossen.onboardingapp.utils.date.DateUtils
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
@@ -20,41 +20,36 @@ import java.time.LocalDate
 class ArticlesScreenViewModelTest {
     private val sourceId = "123"
     private val date = "2024-01-18"
-    private val language = NewsApiService.DEFAULT_LANGUAGE
     private val sortBy = NewsApiService.SORT_BY_POPULAR
-    private val service = mock<NewsApiService>()
+    private val useCase = mock<ArticlesUseCase>()
     private val scheduler = Schedulers.trampoline()
     private val dateUtils = mock<DateUtils>()
     private lateinit var viewModel: ArticlesScreenViewModel
     private val savedStateHandle = SavedStateHandle().apply {
         set(ArticlesScreenViewModel.SOURCE_ID_KEY, sourceId)
     }
-    private val mockArticleResponse = ArticlesResponse(
-        "ok",
-        2,
-        listOf(
-            Article.createSample(),
-            Article.createSample()
-        )
+    private val mockArticles = listOf(
+        Article.createSample(),
+        Article.createSample()
     )
 
     @Before
     fun setup() {
         given(dateUtils.oneDayPast()).willReturn(LocalDate.parse(date))
-        given(service.getArticlesFromSource(sourceId, language, sortBy, date)).willReturn(
-            Single.just(mockArticleResponse)
+        given(useCase.getArticles(sourceId, sortBy, date)).willReturn(
+            Single.just(mockArticles)
         )
         viewModel = createViewModel()
     }
 
     @Test
     fun init_serviceGetsArticles() {
-        verify(service).getArticlesFromSource(sourceId, from = date)
+        verify(useCase).getArticles(sourceId, sortBy, date)
     }
 
     @Test
     fun init_onSuccess_articlesReturned() {
-        Assert.assertEquals(mockArticleResponse.articles, viewModel.articles.value)
+        Assert.assertEquals(mockArticles, viewModel.articles.value)
     }
 
     @Test
@@ -67,14 +62,13 @@ class ArticlesScreenViewModelTest {
     @Test
     fun sortByPopularAll_sortStateEqualsPopularAll() {
         given(
-            service.getArticlesFromSource(
+            useCase.getArticles(
                 sourceId,
-                language,
                 NewsApiService.SORT_BY_POPULAR,
                 null
             )
         ).willReturn(
-            Single.just(mockArticleResponse)
+            Single.just(mockArticles)
         )
 
         viewModel.setSortState(SortState.POPULAR_ALL_TIME)
@@ -85,14 +79,13 @@ class ArticlesScreenViewModelTest {
     @Test
     fun sortByNewest_sortStateEqualsNewest() {
         given(
-            service.getArticlesFromSource(
+            useCase.getArticles(
                 sourceId,
-                language,
                 NewsApiService.SORT_BY_PUBLISHED_AT,
                 null
             )
         ).willReturn(
-            Single.just(mockArticleResponse)
+            Single.just(mockArticles)
         )
 
         viewModel.setSortState(SortState.NEWEST)
@@ -100,5 +93,5 @@ class ArticlesScreenViewModelTest {
         Assert.assertEquals(SortState.NEWEST, viewModel.sortState.value)
     }
 
-    private fun createViewModel() = ArticlesScreenViewModel(service, scheduler, dateUtils, savedStateHandle)
+    private fun createViewModel() = ArticlesScreenViewModel(useCase, scheduler, dateUtils, savedStateHandle)
 }
