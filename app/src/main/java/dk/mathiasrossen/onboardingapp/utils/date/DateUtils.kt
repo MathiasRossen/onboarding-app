7d8c8166fd3dcd49dd626d69a6dbc20643658622
Mathias <mgaj@tv2.dk>
2024-01-25 15:44:03 +0100

    Add default patterns for dates

--- app/src/main/java/dk/mathiasrossen/onboardingapp/utils/date/DateUtils.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/utils/date/DateUtils.kt
@@ -4,4 +4,9 @@ import java.time.LocalDate
 
 interface DateUtils {
     fun oneDayPast(): LocalDate
+
+    companion object {
+        const val DEFAULT_DISPLAY_PATTERN = "yyyy-MM-dd HH:mm"
+        const val DEFAULT_API_PATTERN = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+    }
 }
