c2bd0870781a929d64702f0e5d856beedbc80f7f
Mathias <mgaj@tv2.dk>
2024-01-30 14:15:46 +0100

    Add back button to app bar

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/appbar/OnboardingLargeTopAppBar.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/appbar/OnboardingLargeTopAppBar.kt
@@ -1,6 +1,5 @@
 package dk.mathiasrossen.onboardingapp.ui.appbar
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,19 +15,28 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun OnboardingLargeTopAppBar(title: String) {
+fun OnboardingLargeTopAppBar(
+    title: String,
+    imageUrl: String?,
+    canNavigateBack: Boolean = false,
+    onNavigateUp: () -> Unit
+) {
     Box(modifier = Modifier.fillMaxWidth()) {
-        Image(
+        val placeholderPainter = painterResource(id = R.drawable.image_placeholder)
+        AsyncImage(
+            model = imageUrl,
+            placeholder = placeholderPainter,
+            error = placeholderPainter,
             contentDescription = null,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(dimensionResource(id = R.dimen.base_large_top_app_bar_height)),
-            painter = painterResource(id = R.drawable.image_placeholder),
             contentScale = ContentScale.Crop
         )
         LargeTopAppBar(
@@ -38,7 +46,8 @@ fun OnboardingLargeTopAppBar(title: String) {
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = Color.Transparent,
                 titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-            )
+            ),
+            navigationIcon = { TopAppBarBackButton(canNavigateBack, onNavigateUp) }
         )
     }
 }
@@ -47,6 +56,6 @@ fun OnboardingLargeTopAppBar(title: String) {
 @Composable
 private fun TopAppBarPreview() {
     OnboardingAppTheme {
-        OnboardingLargeTopAppBar("NewsApp")
+        OnboardingLargeTopAppBar("NewsApp", null) {}
     }
 }
