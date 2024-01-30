a673d88540df6a4a06bc0fc6eef0393dd8de753d
Mathias <mgaj@tv2.dk>
2024-01-30 12:22:52 +0100

    Add favorite logic to ArticleItemAuthorRow

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/ArticleItemAuthorRow.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/ArticleItemAuthorRow.kt
@@ -1,24 +1,27 @@
 package dk.mathiasrossen.onboardingapp.ui.articles
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.ui.theme.BlueGray
 import dk.mathiasrossen.onboardingapp.ui.theme.Typography
 
 @Composable
-fun ArticleItemAuthorRow(authorAndDate: String, favorite: Boolean) {
-    val favoriteResource = if (favorite) R.drawable.icon_favorite_active else R.drawable.icon_favorite
+fun ArticleItemAuthorRow(authorAndDate: String, isFavorite: Boolean, onFavoriteClick: () -> Unit) {
+    val favoriteResource = if (isFavorite) R.drawable.icon_favorite_active else R.drawable.icon_favorite
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -26,7 +29,11 @@ fun ArticleItemAuthorRow(authorAndDate: String, favorite: Boolean) {
     ) {
         Text(text = authorAndDate, style = Typography.labelLarge, color = BlueGray)
         Image(
-            modifier = Modifier.size(24.dp),
+            modifier = Modifier
+                .size(dimensionResource(id = R.dimen.base_icon_size))
+                .clickable(onClick = onFavoriteClick, interactionSource = remember {
+                    MutableInteractionSource()
+                }, indication = null),
             painter = painterResource(id = favoriteResource),
             contentDescription = null,
             colorFilter = ColorFilter.tint(
