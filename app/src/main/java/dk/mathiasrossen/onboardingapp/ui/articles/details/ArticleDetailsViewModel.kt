023452f6702304784a41cb9921d4b1c6d6c8075c
Mathias <mgaj@tv2.dk>
2024-02-02 10:51:49 +0100

    Add observeOn uiScheduler when fetching articles

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/details/ArticleDetailsViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/articles/details/ArticleDetailsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.SavedStateHandle
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dk.mathiasrossen.onboardingapp.data.article.Article
 import dk.mathiasrossen.onboardingapp.dependency_injection.annotations.IoScheduler
+import dk.mathiasrossen.onboardingapp.dependency_injection.annotations.UiScheduler
 import dk.mathiasrossen.onboardingapp.use_cases.ArticlesUseCase
 import dk.mathiasrossen.onboardingapp.utils.BaseViewModel
 import io.reactivex.rxjava3.core.Scheduler
@@ -15,6 +16,8 @@ class ArticleDetailsViewModel @Inject constructor(
     private val articlesUseCase: ArticlesUseCase,
     @IoScheduler
     private val ioScheduler: Scheduler,
+    @UiScheduler
+    private val uiScheduler: Scheduler,
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel() {
     private val articleUuid: String = checkNotNull(savedStateHandle[ARTICLE_UUID_KEY])
@@ -29,6 +32,7 @@ class ArticleDetailsViewModel @Inject constructor(
                 .flatMap(
                     { article -> articlesUseCase.getIsArticleFavorite(article) },
                     { article, isFavorite -> article to isFavorite })
+                .observeOn(uiScheduler)
                 .subscribe { articleAndFavoritePair ->
                     articleState.value = articleAndFavoritePair.first
                     favoriteState.value = articleAndFavoritePair.second
