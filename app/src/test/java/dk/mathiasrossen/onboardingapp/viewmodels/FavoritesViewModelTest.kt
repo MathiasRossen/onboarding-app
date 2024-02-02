e7f6d442130e59376d4add2824cb864104ab8691
Mathias <mgaj@tv2.dk>
2024-02-02 10:42:12 +0100

    Add unit test for FavoritesViewModel loadArticles

---
+++ app/src/test/java/dk/mathiasrossen/onboardingapp/viewmodels/FavoritesViewModelTest.kt
@@ -0,0 +1,38 @@
+package dk.mathiasrossen.onboardingapp.viewmodels
+
+import dk.mathiasrossen.onboardingapp.data.article.Article
+import dk.mathiasrossen.onboardingapp.data.article.FavoriteArticle
+import dk.mathiasrossen.onboardingapp.ui.favorites.FavoritesViewModel
+import dk.mathiasrossen.onboardingapp.use_cases.ArticlesUseCase
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.given
+
+class FavoritesViewModelTest {
+    private val scheduler = Schedulers.trampoline()
+    private val useCase = mock<ArticlesUseCase>()
+    private val articleUrl = "horse"
+    private val mockArticles = listOf(Article.createSample(articleUrl), Article.createSample())
+    private val mockFavoriteArticles = listOf(FavoriteArticle(articleUrl))
+    private lateinit var viewModel: FavoritesViewModel
+
+    @Before
+    fun setup() {
+        given(useCase.getFavoriteArticles()).willReturn(Single.just(mockFavoriteArticles))
+        given(useCase.getArticlesByUrls(listOf(articleUrl))).willReturn(Single.just(mockArticles))
+        viewModel = createViewModel()
+    }
+
+    @Test
+    fun loadArticles_favoriteArticlesAreLoaded() {
+        viewModel.loadArticles()
+
+        assertEquals(mockArticles, viewModel.articles.value)
+    }
+
+    private fun createViewModel() = FavoritesViewModel(useCase, scheduler)
+}
