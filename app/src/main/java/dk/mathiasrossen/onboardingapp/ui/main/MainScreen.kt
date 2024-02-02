de3ec04f3ccb6e5ae9e1f22ad534c11637b687b7
Mathias <mgaj@tv2.dk>
2024-02-02 14:56:41 +0100

    Extract appbar titles to string resources

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/main/MainScreen.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/main/MainScreen.kt
@@ -7,11 +7,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.navigation.Routes
 import dk.mathiasrossen.onboardingapp.navigation.Screen
 import dk.mathiasrossen.onboardingapp.ui.about.AboutScreen
@@ -29,7 +31,8 @@ import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
 fun MainScreen(sourcesViewModel: SourcesViewModel) {
     val navController = rememberNavController()
     OnboardingAppTheme {
-        val appBarTitle = remember { mutableStateOf("NewsApp") }
+        val appName = stringResource(R.string.app_name)
+        val appBarTitle = remember { mutableStateOf(appName) }
         val appBarImageUrl = remember { mutableStateOf("") }
         Scaffold(
             topBar = {
@@ -66,7 +69,7 @@ fun MainScreen(sourcesViewModel: SourcesViewModel) {
             ) {
                 navigation(startDestination = Screen.Sources.routeMain, route = Screen.Sources.route) {
                     composable(Screen.Sources.routeMain) {
-                        appBarTitle.value = "NewsApp"
+                        appBarTitle.value = appName
                         SourcesScreen(sourcesViewModel) { sourceId, sourceName ->
                             navController.navigate("${Routes.ARTICLES}/$sourceId?name=$sourceName")
                         }
@@ -83,12 +86,13 @@ fun MainScreen(sourcesViewModel: SourcesViewModel) {
                     }
                 }
                 composable(Screen.Favorites.route) {
-                    appBarTitle.value = "Favorites"
+                    appBarTitle.value = stringResource(R.string.appbar_title_favorites)
                     FavoritesScreen { article ->
                         navController.navigate("articleDetails/${article.uuid}")
                     }
                 }
                 composable(Screen.About.route) {
+                    appBarTitle.value = stringResource(R.string.appbar_title_about)
                     AboutScreen()
                 }
             }
