bb5fcc7414aeee9a5e2fcaac7eeec12b612858a8
Mathias <mgaj@tv2.dk>
2024-02-05 14:02:05 +0100

    Move NewsSourceView into its own file

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourceItem.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourceItem.kt
@@ -3,39 +3,18 @@ package dk.mathiasrossen.onboardingapp.ui.sources
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
 import dk.mathiasrossen.onboardingapp.ui.theme.OnboardingAppTheme
 import dk.mathiasrossen.onboardingapp.ui.theme.Typography
 
 @Composable
-fun SourcesScreen(
-    sourcesViewModel: SourcesViewModel = hiltViewModel(),
-    onNewsSourceClick: (sourceId: String, sourceName: String) -> Unit
-) {
-    val sourcesState by sourcesViewModel.newsSources
-    LazyColumn(
-        contentPadding = PaddingValues(dimensionResource(id = R.dimen.base_content_padding)),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.base_arrangement_space_large))
-    ) {
-        items(newsSources) { newsSource ->
-            SourceItem(newsSource, onNewsSourceClick)
-        }
-    }
-}
-
-@Composable
-fun NewsSourceView(
+fun SourceItem(
     newsSource: NewsSourcesResponse.NewsSource,
     onNewsSourceClick: (sourceId: String, sourceName: String) -> Unit
 ) {
@@ -50,9 +29,9 @@ fun NewsSourceView(
 
 @Preview(showBackground = true)
 @Composable
-fun NewsSourceViewPreview() {
+private fun SourceItemPreview() {
     OnboardingAppTheme {
-        NewsSourceView(
+        SourceItem(
             NewsSourcesResponse.NewsSource(
                 "1",
                 "HorseNews",
