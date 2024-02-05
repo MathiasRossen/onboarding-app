1407ba10ee5c463620e31851851b6eee0b06969b
Mathias <mgaj@tv2.dk>
2024-02-05 14:08:13 +0100

    Change SourcesViewModel inheritance to BaseViewModel

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesViewModel.kt
@@ -1,24 +1,18 @@
 package dk.mathiasrossen.onboardingapp.ui.sources
 
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dk.mathiasrossen.onboardingapp.api.NewsApiService
 import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
 import dk.mathiasrossen.onboardingapp.dependency_injection.annotations.UiScheduler
+import dk.mathiasrossen.onboardingapp.utils.BaseViewModel
 import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.disposables.Disposable
 import javax.inject.Inject
 
 @HiltViewModel
 class SourcesViewModel @Inject constructor(
-    private val newsApiService: NewsApiService,
-    @UiScheduler
-    private val uiScheduler: Scheduler
-) :
-    ViewModel() {
-    private var disposable = Disposable.disposed()
-
+    private val newsApiService: NewsApiService, @UiScheduler private val uiScheduler: Scheduler
+) : BaseViewModel() {
     val isLoading = mutableStateOf(true)
     val newsSources = mutableStateOf(listOf<NewsSourcesResponse.NewsSource>())
     var onError: () -> Unit = {}
@@ -28,19 +22,14 @@ class SourcesViewModel @Inject constructor(
     }
 
     fun loadSources() {
-        disposable = newsApiService.getSources()
-            .observeOn(uiScheduler)
-            .subscribe({ newsSourcesResponse ->
+        compositeDisposable.add(
+            newsApiService.getSources().observeOn(uiScheduler).subscribe({ newsSourcesResponse ->
                 newsSources.value = newsSourcesResponse.sourcesSorted
                 isLoading.value = false
             }) {
                 isLoading.value = false
                 onError()
             }
-    }
-
-    override fun onCleared() {
-        disposable.dispose()
-        super.onCleared()
+        )
     }
 }
