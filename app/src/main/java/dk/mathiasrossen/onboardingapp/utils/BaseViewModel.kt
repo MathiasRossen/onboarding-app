78d8d963bc99bf8a86bf3029ef0e3c901bc10fbd
Mathias <mgaj@tv2.dk>
2024-02-06 09:46:06 +0100

    Add hasError to BaseViewModel

--- app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
@@ -6,6 +6,7 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 open class BaseViewModel(title: String = "NewsApp"): ViewModel() {
     val appBarTitle = mutableStateOf(title)
+    val hasError = mutableStateOf(false)
 
     protected var compositeDisposable = CompositeDisposable()
 
