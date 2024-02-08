3bfd422383b9614d8389b0f312568fa06b0e494c
Mathias <mgaj@tv2.dk>
2024-02-08 10:23:06 +0100

    Add ErrorPromoter to BaseViewModel

--- app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
@@ -2,9 +2,18 @@ package dk.mathiasrossen.onboardingapp.utils
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dk.mathiasrossen.onboardingapp.utils.errors.ErrorPromoter
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import javax.inject.Inject
 
-open class BaseViewModel(title: String = "NewsApp"): ViewModel() {
+@HiltViewModel
+open class BaseViewModel(title: String) : ViewModel() {
+    @Inject
+    constructor() : this("NewsApp")
+
+    @Inject
+    lateinit var errorPromoter: ErrorPromoter
     val appBarTitle = mutableStateOf(title)
     val isLoading = mutableStateOf(false)
 
