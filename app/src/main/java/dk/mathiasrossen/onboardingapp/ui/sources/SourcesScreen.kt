710c5eec013f22bcb3faf39b232006de94815d6f
Mathias <mgaj@tv2.dk>
2024-02-08 09:58:21 +0100

    Remove snackbar errors handling inside SourcesScreen

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesScreen.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesScreen.kt
@@ -4,15 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
 import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
 import dk.mathiasrossen.onboardingapp.ui.base.ErrorScreen
@@ -21,31 +17,19 @@ import dk.mathiasrossen.onboardingapp.ui.base.LoadingScreen
 @Composable
 fun SourcesScreen(
     sourcesViewModel: SourcesViewModel = hiltViewModel(),
-    snackbarHostState: SnackbarHostState,
     onNewsSourceClick: (sourceId: String, sourceName: String) -> Unit
 ) {
-    val hasError by sourcesViewModel.hasError
-    val isLoading by sourcesViewModel.isLoading
-    if (isLoading) {
+    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
+        sourcesViewModel.onResume()
+    }
+    val sources = sourcesViewModel.newsSources.value
+    if (sourcesViewModel.isLoading.value) {
         LoadingScreen()
-    } else if (hasError) {
-        val snackbarMessage = stringResource(R.string.sources_error)
-        val snackbarActionLabel = stringResource(R.string.base_error_retry)
-        LaunchedEffect(snackbarHostState) {
-            val result = snackbarHostState.showSnackbar(
-                message = snackbarMessage,
-                actionLabel = snackbarActionLabel,
-                duration = SnackbarDuration.Indefinite
-            )
-            if (result == SnackbarResult.ActionPerformed) {
-                sourcesViewModel.loadSources()
-            }
-        }
+    } else if (sources.isEmpty()) {
         ErrorScreen()
     } else {
-        SourcesList(newsSources = sourcesViewModel.newsSources.value, onNewsSourceClick = onNewsSourceClick)
+        SourcesList(newsSources = sources, onNewsSourceClick = onNewsSourceClick)
     }
-
 }
 
 @Composable
