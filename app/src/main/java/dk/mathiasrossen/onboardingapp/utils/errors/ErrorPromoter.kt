39cff4540d0624af5a35829c640f3c2f36b4367e
Mathias <mgaj@tv2.dk>
2024-02-08 09:48:23 +0100

    Add ErrorPromoter

---
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/utils/errors/ErrorPromoter.kt
@@ -0,0 +1,24 @@
+package dk.mathiasrossen.onboardingapp.utils.errors
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
+class ErrorPromoter {
+    val errors: MutableState<List<AppError>> = mutableStateOf(listOf())
+
+    fun submitError(error: AppError) {
+        errors.value = errors.value.toMutableList().apply {
+            add(error)
+        }
+    }
+
+    fun dismissError(error: AppError) {
+        errors.value = errors.value.toMutableList().apply {
+            remove(error)
+        }
+    }
+
+    fun dismissAllErrors() {
+        errors.value = listOf()
+    }
+}
