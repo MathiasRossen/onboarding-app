5a851e1d97ca427e797a00b680ffc5435b960da0
Mathias <mgaj@tv2.dk>
2024-02-08 09:54:08 +0100

    Remove hasError from BaseViewModel

--- app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/utils/BaseViewModel.kt
@@ -6,7 +6,6 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 open class BaseViewModel(title: String = "NewsApp"): ViewModel() {
     val appBarTitle = mutableStateOf(title)
-    val hasError = mutableStateOf(false)
     val isLoading = mutableStateOf(false)
 
     protected var compositeDisposable = CompositeDisposable()
