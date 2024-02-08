c9de212ab619743fdf820290b2ccf9d2e5a1db41
Mathias <mgaj@tv2.dk>
2024-02-08 09:49:50 +0100

    Change error handling to ErrorPromoter

--- app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesViewModel.kt
+++ app/src/main/java/dk/mathiasrossen/onboardingapp/ui/sources/SourcesViewModel.kt
@@ -2,36 +2,50 @@ package dk.mathiasrossen.onboardingapp.ui.sources
 
 import androidx.compose.runtime.mutableStateOf
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dk.mathiasrossen.onboardingapp.R
 import dk.mathiasrossen.onboardingapp.api.NewsApiService
 import dk.mathiasrossen.onboardingapp.api.response_models.NewsSourcesResponse
 import dk.mathiasrossen.onboardingapp.dependency_injection.annotations.UiScheduler
 import dk.mathiasrossen.onboardingapp.utils.BaseViewModel
+import dk.mathiasrossen.onboardingapp.utils.errors.ErrorPromoter
+import dk.mathiasrossen.onboardingapp.utils.errors.RetryActionError
 import io.reactivex.rxjava3.core.Scheduler
 import javax.inject.Inject
 
 @HiltViewModel
 class SourcesViewModel @Inject constructor(
-    private val newsApiService: NewsApiService, @UiScheduler private val uiScheduler: Scheduler
+    private val newsApiService: NewsApiService,
+    @UiScheduler
+    private val uiScheduler: Scheduler,
+    private val errorPromoter: ErrorPromoter
 ) : BaseViewModel() {
-    val isLoading = mutableStateOf(true)
-    val hasError = mutableStateOf(false)
     val newsSources = mutableStateOf(listOf<NewsSourcesResponse.NewsSource>())
 
     init {
         loadSources()
     }
 
-    fun loadSources() {
+    private fun loadSources() {
         isLoading.value = true
         compositeDisposable.add(
             newsApiService.getSources().observeOn(uiScheduler).subscribe({ newsSourcesResponse ->
                 newsSources.value = newsSourcesResponse.sourcesSorted
                 isLoading.value = false
-                hasError.value = false
             }) {
                 isLoading.value = false
-                hasError.value = true
+                errorPromoter.submitError(
+                    RetryActionError(
+                        messageStringResource = R.string.sources_error,
+                        retryAction = ::loadSources
+                    )
+                )
             }
         )
+    }
+
+    fun onResume() {
+        if (!isLoading.value && newsSources.value.isEmpty()) {
+            loadSources()
+        }
     }
 }
